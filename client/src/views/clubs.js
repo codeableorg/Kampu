db08322606609db8cc1ff9ecd8ec148be457978e
Lian Nivin <liamrn94@gmail.com>
2019-07-18 11:15:07 -0500

    Add basic component

---
+++ client/src/views/clubs.js
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx } from "@emotion/core";
+import { navigate } from "@reach/router";
+
+function InformationClub() {
+  return "information club";
+}
+
+export default InformationClub;
