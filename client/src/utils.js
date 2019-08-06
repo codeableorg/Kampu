038e8a4917ed82cb668c57efffaa206f5f4060fe
condef5 <davis.con.fab@gmail.com>
2019-08-06 16:20:01 -0500

    Setup url production

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,5 @@
 export const apiUrl =
-  process.env.NODE_ENV === "development"
-    ? process.env.REACT_APP_API_URL
-    : process.env.REACT_APP_API_URL_PRODUCTION;
+  process.env.REACT_APP_API_URL || "http://localhost:4000/api";
 
 export function formatHour(hour) {
   return ("0" + hour.toString() + ":00").slice(-5);
